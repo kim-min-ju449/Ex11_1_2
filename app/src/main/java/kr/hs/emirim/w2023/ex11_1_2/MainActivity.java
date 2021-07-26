3e0dc896c8077b871d18a933b6b516250acaccd2
김민주 <w2023@e-mirim.hs.kr>
2021-07-26 11:46:20 +0900

    Edit MainActivity.java Commit

--- app/src/main/java/kr/hs/emirim/w2023/ex11_1_2/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2023/ex11_1_2/MainActivity.java
@@ -3,12 +3,30 @@ package kr.hs.emirim.w2023.ex11_1_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String[] arr = {"Black Window", "Quiet Place 2","Edge Of Tomorrow","기묘한 이야기","크루엘라","보스베이비2" };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list1 = findViewById(R.id.list1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arr);
+        list1.setAdapter(adapter);
+
+        list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), arr[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
